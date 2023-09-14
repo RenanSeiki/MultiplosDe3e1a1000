4eb1b39e24d5dd362159f8f81e18763760443901
Renan Seiki de Almeida - 823117418 <823117418@ulife.com.br>
2023-09-14 19:39:45 -0300

    first commit

---
+++ MultiplosDe3e1a1000.java
@@ -0,0 +1,7 @@
+public class MultiplosDe3e1a1000{
+    public static void main(String[] args) {
+        System.out.println(" Multiplos de 3 e 1 a 1000 ");
+        for (int cont=3; cont <=1000; cont = cont + 3)
+        System.out.print (cont + " ");
+    }
+}
